2968f1e811f203159cf4628eded4eb95512cb798
Hubert Strękowski <hstrekowski05@gmail.com>
2026-03-29 20:19:32 +0200

    feat(infrastructure) added references, repostiories implementations will be added later

--- src/FlatFlow.Infrastructure/InfrastructureServiceRegistration.cs
+++ src/FlatFlow.Infrastructure/InfrastructureServiceRegistration.cs
@@ -1,10 +1,6 @@
-﻿using FlatFlow.Application.Contracts.Persistence;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace FlatFlow.Infrastructure
 {
@@ -17,11 +13,11 @@ namespace FlatFlow.Infrastructure
 
             // Register repositories - Implementation of repositories will be added later
             // services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-            //services.AddScoped<IChoreRepository, ChoreRepository>();
-            //services.AddScoped<IFlatRepository, FlatRepository>();
-            //services.AddScoped<INoteRepository, NoteRepository>();
-            //services.AddScoped<IPaymentRepository, PaymentRepository>();
-            //services.AddScoped<ITenantRepository, TenantRepository>();
+            // services.AddScoped<IChoreRepository, ChoreRepository>();
+            // services.AddScoped<IFlatRepository, FlatRepository>();
+            // services.AddScoped<INoteRepository, NoteRepository>();
+            // services.AddScoped<IPaymentRepository, PaymentRepository>();
+            // services.AddScoped<ITenantRepository, TenantRepository>();
 
             return services;
         }
