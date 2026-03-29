ac2b83715ff331520539d19d45bcb935f96c8b55
Hubert Strękowski <hstrekowski05@gmail.com>
2026-03-29 20:03:50 +0200

    feat(infrastructure) add DbContext and ServiceRegistration

---
+++ src/FlatFlow.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,29 @@
+﻿using FlatFlow.Application.Contracts.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlatFlow.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<Persistence.FlatFlowDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            // Register repositories - Implementation of repositories will be added later
+            // services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            //services.AddScoped<IChoreRepository, ChoreRepository>();
+            //services.AddScoped<IFlatRepository, FlatRepository>();
+            //services.AddScoped<INoteRepository, NoteRepository>();
+            //services.AddScoped<IPaymentRepository, PaymentRepository>();
+            //services.AddScoped<ITenantRepository, TenantRepository>();
+
+            return services;
+        }
+    }
+}
---
+++ src/FlatFlow.Infrastructure/Persistence/FlatFlowDbContext.cs
@@ -0,0 +1,44 @@
+﻿using FlatFlow.Domain.Common;
+using FlatFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace FlatFlow.Infrastructure.Persistence
+{
+    public class FlatFlowDbContext : DbContext
+    {
+        public FlatFlowDbContext(DbContextOptions<FlatFlowDbContext> options) : base(options) { }
+        public DbSet<Flat> Flats { get; set; }
+        public DbSet<Tenant> Tenants { get; set; }
+        public DbSet<Chore> Chores { get; set; }
+        public DbSet<ChoreAssignment> ChoreAssignments { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<PaymentShare> PaymentShares { get; set; }
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var modifiedEntries = ChangeTracker.Entries<BaseEntity>().Where(e => e.State == EntityState.Modified);
+
+            var methodInfo = typeof(BaseEntity).GetMethod("SetUpdatedAt", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            if (methodInfo != null)
+            {
+                foreach (var entry in modifiedEntries)
+                {
+                    methodInfo.Invoke(entry.Entity, null);
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
